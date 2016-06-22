74295e0db9b736aa4ee2934ee851241bfdbe3dba
mitri4 <mitri4@bk.ru>
2016-06-22 21:30:45 +0300

    Начало скрипта для выпадайки

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+
+    console.log('start');
+
+    var navigateDroupdownsItems = document.getElementsByClassName('nav__item_dropdown');
+
+
+    for (var i=0; i < navigateDroupdownsItems.length; i++) {
+        var changeStatus = function () {
+            console.log(this);
+            this.classList.toggle('dropdown_is-active');
+        };
+        
+        navigateDroupdownsItems[i].addEventListener('mouseover', changeStatus, 'false');
+        navigateDroupdownsItems[i].addEventListener('mouseout', changeStatus, 'false');
+    }
+    
+};
+
+
+
+
