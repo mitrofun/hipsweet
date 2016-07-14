560e15c3115a969b7adb73f1b7a5e62487673357
mitri4 <mitri4@bk.ru>
2016-07-14 19:47:22 +0300

    del js dropdown menu, css hover dropdown menu header

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,5 @@
 $(document).ready(function() {
-
-    console.log('start');
-
-    var navigateDroupdownsItems = document.querySelectorAll('.nav__item_dropdown');
-    var changeStatus = function () {
-            console.log(this);
-            this.classList.toggle('dropdown_is-active');
-        };
-
-    for (var i=0; i < navigateDroupdownsItems.length; i++) {
-        navigateDroupdownsItems[i].addEventListener('mouseover', changeStatus, 'false');
-        navigateDroupdownsItems[i].addEventListener('mouseout', changeStatus, 'false');
-    }
-
+    
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true
