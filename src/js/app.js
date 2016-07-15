9e9b13f07f16a7f5139a0b7347547fa821ea2b62
mitri4 <mitri4@bk.ru>
2016-07-15 19:19:10 +0300

    js anonymous function

--- src/js/app.js
+++ src/js/app.js
@@ -1,50 +1,61 @@
 $(document).ready(function() {
     
-    var swiper = new Swiper('.swiper-container', {
+    (function () {
+        var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true
-    });
+        });
+    })();
 
-    $('.deliver__send_link').click( function(e){
-    	e.preventDefault();
-	    $("html, body").delay(200).animate({scrollTop: $('#send_mail').offset().top }, 2000);
-    });
+    (function () {
+        $('.deliver__send_link').click( function(e){
+            e.preventDefault();
+            $("html, body").delay(200).animate({scrollTop: $('#send_mail').offset().top }, 2000);
+        });}
+    )();
 
-	var tabs = $('.team__tabs');
-	var paragraphs = $('.contributer__description');
+    (function () {
+        var tabs = $('.team__tabs');
+        var paragraphs = $('.contributer__description');
 
-	tabs.on('click', function(event) {
-        console.log(event);
-		event.preventDefault();
-		tabs.removeClass('team__tabs_active');
-		paragraphs.removeClass('contributer__description_active');
-		$(this).addClass('team__tabs_active');
-		var tab = this;
-		tabs.each(function(index, el) {
-			if (el === tab) {
-				$(paragraphs[index]).addClass('contributer__description_active');
-			}
-		});
-	});
+        tabs.on('click', function(event) {
+            console.log(event);
+            event.preventDefault();
+            tabs.removeClass('team__tabs_active');
+            paragraphs.removeClass('contributer__description_active');
+            $(this).addClass('team__tabs_active');
+            var tab = this;
+            tabs.each(function(index, el) {
+                if (el === tab) {
+                    $(paragraphs[index]).addClass('contributer__description_active');
+                }
+            });
+        });
+    })();
 
-    var phone = $('.send-mail_phone');
-    
-    phone.inputmask({'mask':'8 (999) 999-99-99'});
 
-    var questions = $('.questions__item');
+    (function () {
+        var phone = $('.send-mail_phone');
+        phone.inputmask({'mask':'8 (999) 999-99-99'});
+    })();
 
-	questions.on('click', function() {
+    (function () {
+        var questions = $('.questions__item');
+
+	    questions.on('click', function() {
 		console.log(this);
 		questions.removeClass('questions__item_active');
 		$(this).addClass('questions__item_active');
 	});
+    })();
 
 });
 
-ymaps.ready(init);
+(function () {
+    ymaps.ready(init);
     var myMap;
 
-    function init(){     
+    function init() {
         myMap = new ymaps.Map("map__content", {
             center: [58.612137, 49.589505],
             zoom: 16,
@@ -61,3 +72,4 @@ ymaps.ready(init);
         myMap.geoObjects.add(myPlacemark);
         myMap.behaviors.disable('scrollZoom');
     }
+})();
